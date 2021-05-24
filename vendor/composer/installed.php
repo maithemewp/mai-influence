8459fdfd29f6c811cbfb4e485f4bd9c1bc67049e
Mike Hemberger <mike@thestizmedia.com>
2021-05-24 15:08:37 -0400

    update packages

---
+++ vendor/composer/installed.php
@@ -0,0 +1,52 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8e3c553ab0011878709a5b8cad7a3e00feff12ec',
+    'name' => 'maithemewp/mai-influence',
+  ),
+  'versions' => 
+  array (
+    'afragen/wp-dependency-installer' => 
+    array (
+      'pretty_version' => '3.1.0',
+      'version' => '3.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '65570a1eb966bb9adfc9d9e521357da080fbdc5a',
+    ),
+    'collizo4sky/persist-admin-notices-dismissal' => 
+    array (
+      'pretty_version' => '1.4.4',
+      'version' => '1.4.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '900739eb6b0ec0210465f5983a6d4e0e420289e4',
+    ),
+    'maithemewp/mai-influence' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8e3c553ab0011878709a5b8cad7a3e00feff12ec',
+    ),
+    'maithemewp/mai-installer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'e6bff471a1d542c848f7247fe3cf018836c2204f',
+    ),
+  ),
+);
