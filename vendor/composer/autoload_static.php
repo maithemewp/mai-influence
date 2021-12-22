3468af7c544fde597e153e083193b5e466ce3cd6
Mike Hemberger <mike@thestizmedia.com>
2021-12-21 19:44:09 -0500

    make sure wpdi is loaded from engine

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -12,9 +12,6 @@ class ComposerStaticInit069c495dc398afdfac9607626f997184
 
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
-        'WP_Dependency_Installer' => __DIR__ . '/..' . '/afragen/wp-dependency-installer/wp-dependency-installer.php',
-        'WP_Dependency_Installer_Skin' => __DIR__ . '/..' . '/afragen/wp-dependency-installer/wp-dependency-installer-skin.php',
-        'WP_Dismiss_Notice' => __DIR__ . '/..' . '/afragen/wp-dismiss-notice/wp-dismiss-notice.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,29 +6,17 @@
     'aliases' => 
     array (
     ),
-    'reference' => '034ac62a8bdd7b0acc1d226dad79494f9e433126',
+    'reference' => '06b2e78d23455975aa5331b4b4cad54ff203ee7e',
     'name' => 'maithemewp/mai-influence',
   ),
   'versions' => 
   array (
     'afragen/wp-dependency-installer' => 
     array (
-      'pretty_version' => '4.2.2',
-      'version' => '4.2.2.0',
-      'aliases' => 
+      'replaced' => 
       array (
+        0 => '*',
       ),
-      'reference' => '5e5419348c6cde7b02f3c33fb3fc3381062443a1',
-    ),
-    'afragen/wp-dismiss-notice' => 
-    array (
-      'pretty_version' => 'dev-main',
-      'version' => 'dev-main',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => '21949732f1ea21d81500bdda8f882ac7eb0f98af',
     ),
     'maithemewp/mai-influence' => 
     array (
@@ -37,7 +25,7 @@
       'aliases' => 
       array (
       ),
-      'reference' => '034ac62a8bdd7b0acc1d226dad79494f9e433126',
+      'reference' => '06b2e78d23455975aa5331b4b4cad54ff203ee7e',
     ),
     'maithemewp/mai-installer' => 
     array (
@@ -47,7 +35,7 @@
       array (
         0 => '9999999-dev',
       ),
-      'reference' => '244cf7604a6b34a4662c29385497d4257f175956',
+      'reference' => '1b637076d114af7762855493b9bac009a6272d5c',
     ),
   ),
 );
